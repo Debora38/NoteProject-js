93f077f35b091c956fc28ac1cc82947b6c3af135
debora <debora.piu92@gmail.com>
2019-02-19 15:44:33 +0000

    <ul></ul> html list updated

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -6,11 +6,11 @@
   };
 
   NoteListView.prototype.printTitles = function() {
-    var titles = ""
+    var titles = "<ul>"
     for (var i in this.notelist.note_list) {
       titles += "<div><li>" + this.notelist.note_list[i].text + "</li></div>";
     }
-    return titles
+    return titles + "</ul>"
   }
 
 exports.NoteListView = NoteListView;
--- test/note-list-view-test.js
+++ test/note-list-view-test.js
@@ -5,7 +5,8 @@
     var noteListView = new NoteListView();
     noteListView.notelist.note_list.push(new Note("Favourite food: pesto"))
     noteListView.notelist.note_list.push(new Note("Favourite drink: seltzer"))
-    assert.isTrue(noteListView.printTitles() === "<div><li>Favourite food: pesto</li></div><div><li>Favourite drink: seltzer</li></div>")
+    console.log(noteListView.printTitles())
+    assert.isTrue(noteListView.printTitles() === "<ul><div><li>Favourite food: pesto</li></div><div><li>Favourite drink: seltzer</li></div></ul>")
     console.log("TestPrintTitle is passing")
   }
 
@@ -13,7 +14,7 @@
 
   function TestPrintWithoutTitle() {
     var noteListView = new NoteListView();
-    assert.isTrue(noteListView.printTitles() === "")
+    assert.isTrue(noteListView.printTitles() === "<ul></ul>")
     console.log("TestPrintWithoutTitle is passing")
   }
 
