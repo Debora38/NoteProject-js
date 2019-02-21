024475d7c64bd9e171cd046e6eab5307b9470192
debora <debora.piu92@gmail.com>
2019-02-21 15:12:04 +0000

    single note view has method print single note html

--- src/single-note-view.js
+++ src/single-note-view.js
@@ -2,7 +2,11 @@
 
 (function(exports){
   function singleNoteView(){
+    this.note = new Note();
+  }
 
+  singleNoteView.prototype.printSingleNoteHTML = function(){
+    return "<div>" + this.note.text + "</div>"
   }
 
   exports.singleNoteView = singleNoteView
--- test/single-note-view-test.js
+++ test/single-note-view-test.js
@@ -8,5 +8,12 @@
 
   TestSingleNoteView();
 
-  
+  function TestPrintSingleNoteHTML() {
+    var singlenoteview = new singleNoteView();
+    singlenoteview.note.text = 'Favourite drink: seltzer'
+    assert.isTrue(singlenoteview.printSingleNoteHTML() === '<div>Favourite drink: seltzer</div>')
+    console.log('TestPrintSingleNoteHTML is passing')
+  }
+
+  TestPrintSingleNoteHTML();
 })(this);
