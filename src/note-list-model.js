3ae5311fd7225335dad2494ea4adb0611b2e325e
debora <debora.piu92@gmail.com>
2019-02-23 20:16:58 +0000

    useless functionality printlist

--- src/note-list-model.js
+++ src/note-list-model.js
@@ -5,14 +5,6 @@
     this.all = []
   };
 
-NoteList.prototype.printList = function() {
-  var list = ""
-  for (var i in this.all) {
-    list += this.all[i] + " ";
-  }
-  return list.trim()
-};
-
 NoteList.prototype.create = function(text) {
   var note = new Note(text)
   this.all.push(note)
