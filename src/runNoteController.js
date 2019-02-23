7574280558a79961ad46b967b7cbded7261af20a
debora <debora.piu92@gmail.com>
2019-02-23 20:31:34 +0000

    list instantiated in runcontroller

--- src/runNoteController.js
+++ src/runNoteController.js
@@ -1,6 +1,5 @@
-var notecontroller = new NoteController();
+var list = new NoteList();
+var notecontroller = new NoteController(list);
 notecontroller.view.list.create('Favourite drink: seltzer')
 notecontroller.view.list.create('Another note')
 notecontroller.enterList('app');
-
-notecontroller.makeUrlShowSingleNote();
