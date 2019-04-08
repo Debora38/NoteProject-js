c0f6618001fa01e62b0017ba8a123df60b6ddaaa
Debora38 <debora.piu92@gmail.com>
2019-04-08 12:25:20 +0100

    id numbers fixed in tests

--- test/note-list-view-test.js
+++ test/note-list-view-test.js
@@ -6,7 +6,7 @@
     var noteListView = new NoteListView(notelist);
     noteListView.list.all.push(new Note("Favourite food: pesto"))
     noteListView.list.all.push(new Note("Favourite drink: seltzer"))
-    assert.isTrue(noteListView.printTwentyChar() === "<ul><div><li><a href='#notes/4'>Favourite food: pest</a></li></div><div><li><a href='#notes/5'>Favourite drink: sel</a></li></div></ul>")
+    assert.isTrue(noteListView.printTwentyChar() === "<ul><div><li><a href='#notes/5'>Favourite food: pest</a></li></div><div><li><a href='#notes/6'>Favourite drink: sel</a></li></div></ul>")
     console.log("TestPrintTwentyChar is passing")
   }
 
